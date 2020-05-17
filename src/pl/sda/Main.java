43fbde2eded919b6b4ef6b6fe43c5555bee8fdb9
Jakub <kubplon@gmail.com>
2020-05-17 08:51:59 +0200

    Initial commit

---
+++ src/pl/sda/Main.java
@@ -0,0 +1,23 @@
+package pl.sda;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Stwórz grę w kolko i krzyzyk, matrycę oprzyj o tablice dwuwymiarową. Drugi gracz to komputer - jego odpowiedzi będą generowały się losowo
+         * 1. Stwórz tablicę 3 x 3 - to nasza plansza
+         * 2. Stwórz funkcję wyświetlającą tablicę 3 x 3
+         * 3. Zapytaj użytkownika czy chce być "x" czy "o" - zapisz wybór
+         * 4. Wyświetl użytkownikowi pustą planszę:
+         * | | | |
+         * | | | |
+         * | | | |
+         * 5. Zapytaj użytkownika, które pole chce zaznaczyć (jeśli wybrał kółka) - powinien wprowadzić pozycję x i y. Na przykład:
+         * Wprowadź pozycję x i y:
+         * 1,1
+         * |o| | |
+         * | | | |
+         * | | | |
+         * 6. Jeśli użytkownik wybrał "x" to zaczyna komputer - wylosuj na której współrzędnej wprowadzi znak
+         */
+    }
+}
